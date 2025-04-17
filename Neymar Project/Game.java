db6ae9b77be31b7ddd0a1649dc82bfbef818abbf
Camila <cm118@wellesley.edu>
2025-04-17 18:55:47 -0400

    this is cami

--- Neymar Project/Game.java
+++ Neymar Project/Game.java
@@ -16,6 +16,9 @@ import javafx.geometry.Pos;
 import javafx.scene.layout.*;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.Background;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
 
 public class Game extends Application {
     private Stage primaryStage;
@@ -65,8 +68,8 @@ public class Game extends Application {
         Button playerDinoButton = new Button("Start Game");
         Button playerBalletButton = new Button("Start Game");
         Button playerNeymarButton = new Button("Start Game");
-        
-       
+    
+
         
 
         //set all buttons to lead to "chooseMessiRonaldo" screen
@@ -81,25 +84,27 @@ public class Game extends Application {
         // HIII THIS IS CAMI
         
         //bottom panel
-        HBox bottomPane = new HBox(20, playerDinoButton, playerBalletButton, playerNeymarButton );
         //add the changing sizes and padding here**
         
-        BackgroundImage myImage= new BackgroundImage(new Image("second.png",800, 450,false, true),
+        BackgroundImage myImage= new BackgroundImage(new Image("first.png",800, 450,false, true),
         BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
         BackgroundSize.DEFAULT);
-       
+        
+        HBox bottomPane = new HBox(80, playerDinoButton, playerBalletButton, playerNeymarButton );
+
         //whole
-        //VBox root = new VBox(bottomPane);
+        VBox root = new VBox(bottomPane);
         //root.setSpacing(10);
         //root.setStyle("-fx-padding: 10;");
         
         
 
         BorderPane myNewPane = new BorderPane();
+        
         //root.setBackground(new Background(myImage));
         
         
-        myNewPane.setAlignment(bottomPane, Pos.CENTER);
+        bottomPane.setAlignment(Pos.BOTTOM_CENTER);
 
         myNewPane.setBackground(new Background(myImage));
         myNewPane.setCenter(bottomPane);
@@ -147,22 +152,37 @@ public class Game extends Application {
         saveRonaldo.setOnAction(e -> characterSaveAction("Ronaldo"));
         
         
-        //top panel
-        HBox topPane = new HBox( char1, char2 );
-        //add the changing sizes and padding here**
+        
+        BackgroundImage myImage2= new BackgroundImage(new Image("third.png",800, 450,false, true),
+        BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+        BackgroundSize.DEFAULT);
+        
+        //HBox topPane = new HBox( char1, char2 );
+        
+        
+        
+        BorderPane myNewPane2 = new BorderPane();
         
         //bottom panel
-        HBox bottomPane = new HBox( 20, saveMessi, saveRonaldo );
+        HBox bottomPane2 = new HBox( 95, saveMessi, saveRonaldo );
+        bottomPane2.setAlignment(Pos.BOTTOM_CENTER);
         //add the changing sizes and padding here**
+        myNewPane2.setBackground(new Background(myImage2));
+        
+        myNewPane2.setCenter(bottomPane2);
+        
+        
+        
+        //root.setBackground(new Background(myImage));
         
         
         //whole
-        VBox root = new VBox(topPane, bottomPane);
-        root.setSpacing(10);
-        root.setStyle("-fx-padding: 10;");
+        //VBox root = new VBox(topPane, bottomPane);
+        //root.setSpacing(10);
+        //root.setStyle("-fx-padding: 10;");
         
         //sizing of main box application*
-        return new Scene(root, 400, 300);
+        return new Scene(myNewPane2, 800, 450);
         
     }
     private void characterSaveAction(String name){
@@ -189,28 +209,27 @@ public class Game extends Application {
         //Left pane buttons menu
         Button mr1 = new Button("Math Room 1");
         Button mr2 = new Button("Math Room 2");
-        
-<<<<<<< HEAD
-        VBox roomMenuBar = new VBox(20, mr1, mr2);
-=======
-        mr1.setOnAction(e -> createMathRoom("m1"));
-        mr2.setOnAction(e -> createMathRoom("m2"));
-        
-        VBox roomMenuBar = new VBox(mr1, mr2);
->>>>>>> b52ea7e76747d284221fffc23d3c557c5be082ed
+  //BEN IM COMMENTING THIS OUT SO that I CAN COMPILE      
+//<<<<<<< HEAD
+//VBox roomMenuBar = new VBox(20, mr1, mr2);
+//=======
+//   mr1.setOnAction(e -> createMathRoom("m1"));
+//    mr2.setOnAction(e -> createMathRoom("m2"));
+//    VBox roomMenuBar = new VBox(mr1, mr2);
+//>>>>>>> b52ea7e76747d284221fffc23d3c557c5be082ed
         
         
         //Right pane
         VBox rightPane = new VBox();
         
         
-        //whole
-        HBox root = new HBox(healthBar, roomMenuBar, rightPane);
-        root.setSpacing(10);
-        root.setStyle("-fx-padding: 10;");
+        //whole THIS HAD A MENUBAR IN THE MIDDLE
+    HBox root = new HBox(healthBar, rightPane);
+     //   root.setSpacing(10);
+       // root.setStyle("-fx-padding: 10;");
         
         //sizing of main box application*
-        return new Scene(root, 400, 300);
+        return new Scene(root, 800, 450);
         
     }
     //main room methods
@@ -252,7 +271,7 @@ public class Game extends Application {
         root.setStyle("-fx-padding: 10;");
         
         //sizing of main box application*
-        return new Scene(root, 400, 300);
+        return new Scene(root, 800, 450);
         
     }
     private void mathRoomReset(MathRoom m1){
