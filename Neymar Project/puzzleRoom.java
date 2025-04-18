a89be7a76417b85bf5838feaa1430d927b5a8122
Camila <cm118@wellesley.edu>
2025-04-18 12:43:50 -0400

    puzzle room progress

--- Neymar Project/puzzleRoom.java
+++ Neymar Project/puzzleRoom.java
@@ -7,6 +7,7 @@
  */
 public class puzzleRoom extends Room{
     private int level;
+    private String checkAnswer;
     
     public puzzleRoom(String x, boolean k, int difficultyLevel){
         super(x,k);
@@ -15,6 +16,10 @@ public class puzzleRoom extends Room{
         this.keyStatus = k;
         this.level= difficultyLevel;
     }
-
+    public String checkAnswer(String answer){
+        this.checkAnswer= answer;
+        
+        return answer;
+    }
 }
     
