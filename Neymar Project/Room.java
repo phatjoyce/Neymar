b682bf70266c8833dd72fe06a5f67661fc6aa78a
Benjamin Fang <bfang1@babson.edu>
2025-04-20 20:03:51 -0400

    hi

--- Neymar Project/Room.java
+++ Neymar Project/Room.java
@@ -5,28 +5,15 @@ public class Room
 {
     private String name;
     private String description;
-    
-    //private ArrayList<String> questionBank;
-    //private ArrayList<String> answerBank;
-    //move these to subclass
-    
-    //private ArrayList<Key> inventory;
     private boolean keyStatus;
     
     public Room(String x, boolean k)
     {
         this.name = x;
         this.keyStatus = k;
-        //this.inventory = new ArrayList<Key>();
-        //this.inventory.add(k);
-        
     }
     
-    //only ever needs to remove, and removes at index 0
     public void removeKey() {
-        //inventory.remove(0);
+        keyStatus = false;
     }
-    
-
-    
 }
